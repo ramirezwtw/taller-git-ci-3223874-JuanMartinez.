bd6e0dc9598f33e1eccc18e64d70f7698e71fbb0
ramirezwtw <juanmartinezidk@gmail.com>
2025-12-04 06:57:49 -0500

    feat: implementar funcion agregarItem

--- src/app.js
+++ src/app.js
@@ -2,4 +2,16 @@ function suma(a, b) {
     return a + b;
 }
 
+const lista = document.getElementById('lista');
+
+function agregarItem(texto) {
+    const nuevoItem = document.createElement('li');
+    nuevoItem.textContent = texto;
+    lista.appendChild(nuevoItem);
+}
+
+document.getElementById('agregarBtn').addEventListener('click', () => {
+    agregarItem(`Ítem agregado a las ${new Date().toLocaleTimeString()}`);
+});
+
 console.log('App lista');
